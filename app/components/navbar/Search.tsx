620dc1a7bb14a8136faf3d1d1c2ec215fc7a1d34
Abhishek Ranjan <abhishekrn@proton.me>
2023-04-06 09:56:31 +0530

    add search items

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import {BiSearch} from 'react-icons/bi'
 const Search = () => {
     return (
         <div className="
@@ -40,6 +41,28 @@ const Search = () => {
         ">
             Any Week
 
+        </div>
+        <div className="
+        text-sm
+        pl-6
+        pr-2
+        text-gray-600
+        flex
+        flex-row
+        items-center
+        gap-3
+        "
+        >
+            <div className="hidden sm:block"> Add Guest </div>
+            <div className="
+            p-2
+            bg-rose-500
+            rounded-full
+            text-white
+            ">
+                <BiSearch size={18} />
+
+            </div>
         </div>
 
             </div>
