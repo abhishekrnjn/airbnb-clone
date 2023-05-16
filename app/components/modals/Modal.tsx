359c942dd17d06f68528dd6a85d95f89b4c74e0f
Abhishek Ranjan <abhishekrn@proton.me>
2023-05-16 13:31:22 +0530

    add model body and footer

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useCallback, useEffect, useState } from 'react';
 import {IoMdClose} from 'react-icons/io';
+import Button from '../Button';
 interface ModalProps{
     isOpen?: boolean;
     onClose: () => void;
@@ -138,6 +139,30 @@ const Modal: React.FC<ModalProps> = ({
                                 </button>
                                 <div className = "text-lg font-semibold">
                                     {title}
+                                </div>
+                            </div>
+                            {/* BODY */}
+                            <div className="
+                            relative
+                            p-6
+                            flex-auto
+                            ">
+                                {body}
+                            </div>
+                            {/* FOOTER */}
+                            <div className="
+                            flex
+                            flex-col
+                            gap-2
+                            p-6">
+                                <div className="
+                                flex
+                                flex-row
+                                items-center
+                                gap-4
+                                w-full
+                                ">
+                                    <Button />
                                 </div>
 
                             </div>
