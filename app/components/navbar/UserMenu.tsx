969790d16b3bc1d9c2e7c00748b5fbd826f0f367
Abhishek Ranjan <abhishekrn@proton.me>
2023-06-02 16:45:15 +0530

    changes

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,7 +3,11 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "../hooks/useRegisterModal";
+
+
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -75,7 +79,7 @@ const UserMenu = () => {
                 />
                 <MenuItem 
                   label="Sign up" 
-                  onClick={()=>{}}
+                  onClick={registerModal.onOpen}
                 />
               </>
           </div>
