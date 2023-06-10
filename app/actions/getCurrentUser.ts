c3faa0f60fcb72717b965a4511ad184d93decf9e
Abhishek Ranjan <abhishekrn@proton.me>
2023-06-10 09:52:17 +0530

    add default fn

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -6,7 +6,7 @@ import prisma from "@/app/libs/prismadb";
 export async function getSession(){
     return await getServerSession(authOptions);
 }
-export async function getCurrentUser(){
+export default async function getCurrentUser(){
     try{
         const session = await getSession();
 
